40ffa0f356507c8e4f9c670eab4d4f15c3bd67b7
Paulo Junior <npaulo.ce@gmail.com>
2024-02-27 20:12:05 -0300

    Aula React JS calculadora parte 1 27/02

--- turma3/calculadora-project/src/components/Calculator.jsx
+++ turma3/calculadora-project/src/components/Calculator.jsx
@@ -2,16 +2,16 @@ import React, { useState } from 'react'
 import './Calculator.css'
 
 const Calculator = () => {
-    const [ currentValue, setCurrentValue ] = useState('0')
-    const [ pendingOperator, setPendingOperator ] = useState(null)
-    const [ pendingValue, setPendingValue ] = useState(null)
-    const [ completeOperation, setCompleteOperation ] = useState('')
-    
-    const keyPadNumber = [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ]
-    const operations = [ '+', '-', '*', '/' ]
+    const [currentValue, setCurrentValue] = useState('0')
+    const [pendingOperator, setPendingOperator] = useState(null)
+    const [pendingValue, setPendingValue] = useState(null)
+    const [completeOperation, setCompleteOperation] = useState('')
+
+    const keyPadNumber = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    const operations = ['+', '-', '*', '/']
 
     const handleClick = (val) => {
-        setCurrentValue( prevValue => {
+        setCurrentValue(prevValue => {
             if (prevValue === '0') {
                 return val
             } else {
@@ -21,11 +21,80 @@ const Calculator = () => {
         setCompleteOperation(prevOperation => prevOperation + val)
     }
 
-  return (
-    <div>
-        <h2>Calculadora</h2>
-    </div>
-  )
+    const handleOperation = (operation) => {
+        setCompleteOperation(currentValue + " " + operation)
+        setPendingOperator(operation)
+        setPendingValue(currentValue)
+        setCurrentValue('0')
+    }
+
+    const handleCalculate = () => {
+        if (!pendingOperator || !pendingValue) {
+            return
+        }
+        const num1 = parseFloat(pendingValue)
+        const num2 = parseFloat(currentValue)
+
+        let result
+
+        switch (pendingOperator) {
+            case '+':
+                result = num1 + num2
+                break
+            case '-':
+                result = num1 - num2
+                break
+            case '*':
+                result = num1 * num2
+                break
+            case '/':
+                if (num2 !== 0) {
+                    result = num1 / num2
+                } else {
+                    setCurrentValue('Error')
+                    setCompleteOperation('Error')
+                    setPendingOperator(null)
+                    setPendingValue(null)
+                    return
+                }
+                break
+            default:
+                return
+        }
+        setCompleteOperation(pendingValue + " " + pendingOperator + " " + currentValue + " = " + result)
+    }
+
+    const handleClear = () => {
+        setCurrentValue('0')
+        setPendingOperator(null)
+        setPendingValue(null)
+        setCompleteOperation('')
+    }
+
+    return (
+        <div>
+            <h2>Calculadora</h2>
+            <div>
+                <div>{ completeOperation }</div>
+                <div>{ currentValue }</div>
+                <div>
+                    <button type='button' onClick={handleClear}>AC</button>
+                    {keyPadNumber.map((num) =>
+                        <button 
+                            key={num} 
+                            onClick={() => handleClick(num)}>{num}
+                        </button>
+                    )}
+                    {operations.map((operation) => 
+                        <button
+                            key={operation}
+                            onClick={() => handleOperation(operation)}>{ operation }</button>
+                    )}
+                    <button onClick={handleCalculate}>=</button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Calculator
